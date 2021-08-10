d4182efd5ffdf491664745916c8aa0f2f931971f
loganras <logan.razclan1@gmail.com>
2021-08-10 16:21:46 -0600

    Add routes for diablo game data

--- server/routeControllers/diabloDataController.js
+++ server/routeControllers/diabloDataController.js
@@ -10,6 +10,22 @@ async function getCharacterClasses(req, res, next) {
   }
 }
 
+async function getCharacterClassById(req, res, next) {
+  try {
+    const classId = req.params.id
+    const diabloClass = await CharacterClass.findById(classId)
+
+    if (!diabloClass) {
+      res.status(404)
+      throw new Error('diablo class was not found')
+    }
+
+    res.json(diabloClass)
+  } catch (error) {
+    next(error)
+  }
+}
+
 async function getAttributes(req, res, next) {
   try {
     const attributes = await Attributes.find({})
@@ -19,4 +35,4 @@ async function getAttributes(req, res, next) {
   }
 }
 
-export { getCharacterClasses, getAttributes }
+export { getCharacterClasses, getCharacterClassById, getAttributes }
--- server/routes/diabloDataRoutes.js
+++ server/routes/diabloDataRoutes.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import {
   getAttributes,
+  getCharacterClassById,
   getCharacterClasses,
 } from '../routeControllers/diabloDataController.js'
 
@@ -8,5 +9,6 @@ const router = express.Router()
 
 router.get('/attributes', getAttributes)
 router.get('/classes', getCharacterClasses)
+router.get('/classes/:id', getCharacterClassById)
 
 export default router
